cd340ca202ad98967872cf48b00586411c30f156
Mohammed Amer <mdamer687@gmail.com>
2024-06-01 19:37:12 +0530

    Connected with DB

---
+++ db/connect.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose')
+
+const connectDB = (url)=>{
+    return mongoose.connect(url,console.log("connected"));
+}
+module.exports = connectDB
--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("hi")
+require('dotenv').config()
+const express = require('express')
+const connectDB = require('./db/connect')
+const app = express()
+
+const port = process.env.PORT || 5000
+const start = async()=>{
+    try
+    {
+        await connectDB(process.env.MONO_URL)
+       return  app.listen(port,console.log(`server is listing on port  ${port}`))
+    }
+    catch(error)
+    {
+        console.log(error)
+    }
+}
+
+start()
+console.log("hd")
